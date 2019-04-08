66b93e2a3ec3adca134f8b90a90e715a3482175d
mostafa hamed <mos.hamed11@hotmail.com>
2019-04-08 14:48:11 +0200

    Add project files.

---
+++ Azure3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Azure3.Startup))]
+namespace Azure3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
